6159e20b3addc969456d311bc3e4ec2a1f41ee69
Alira13 <k.bezglasnaya@glorax.com>
2023-08-01 14:59:57 +0300

    Review bugs fixed

--- app/src/main/java/com/example/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/example/playlistmaker/MainActivity.kt
@@ -11,15 +11,15 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        setButtonOnClickListener(R.id.search_button, SearchActivity())
-        setButtonOnClickListener(R.id.media_button, MediaActivity())
-        setButtonOnClickListener(R.id.settings_button, SettingsActivity())
+        setButtonOnClickListener(R.id.search_button, SearchActivity::class.java)
+        setButtonOnClickListener(R.id.media_button, MediaActivity::class.java)
+        setButtonOnClickListener(R.id.settings_button, SettingsActivity::class.java)
     }
 
-    private fun <T : AppCompatActivity> setButtonOnClickListener(buttonId: Int, activity: T) {
+    private fun <T : AppCompatActivity> setButtonOnClickListener(buttonId: Int, activity: Class<T>) {
         val button = findViewById<Button>(buttonId)
         val buttonClickListener = View.OnClickListener {
-            val displayIntent = Intent(this, activity::class.java)
+            val displayIntent = Intent(this, activity)
             startActivity(displayIntent)
         }
         button.setOnClickListener(buttonClickListener)
