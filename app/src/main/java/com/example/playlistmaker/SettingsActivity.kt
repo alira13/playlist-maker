f56c56a02ceebc13e703e80820a06b0afacb6a09
Alira13 <bezglasnaya@gmail.com>
2023-08-09 14:26:33 +0300

    Homework 2 Sprint 9 Set intents for Settings

--- app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
@@ -2,6 +2,7 @@ package com.example.playlistmaker
 
 import android.annotation.SuppressLint
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.View
 import android.widget.ImageButton
@@ -19,5 +20,35 @@ class SettingsActivity : AppCompatActivity() {
             startActivity(displayIntent)
         }
         button.setOnClickListener(buttonClickListener)
+
+        val shareButton = findViewById<ImageButton>(R.id.share_button)
+        val shareButtonClickListener = View.OnClickListener {
+            val shareIntent = Intent(Intent.ACTION_SEND)
+            val message = "https://practicum.yandex.ru/android-developer/?from=catalog"
+            shareIntent.putExtra(Intent.EXTRA_TEXT, message)
+            startActivity(shareIntent)
+        }
+        shareButton.setOnClickListener(shareButtonClickListener)
+
+        val supportButton = findViewById<ImageButton>(R.id.support_button)
+        val supportButtonClickListener = View.OnClickListener {
+            val supportIntent = Intent(Intent.ACTION_SENDTO)
+            val email="Bezglasnaya@gmail.com"
+            val emailSubject="Сообщение разработчикам и разработчицам приложения Playlist Maker"
+            val message = " «Спасибо разработчикам и разработчицам за крутое приложение!»"
+            supportIntent.putExtra(Intent.EXTRA_EMAIL, email)
+            supportIntent.putExtra(Intent.EXTRA_SUBJECT, emailSubject)
+            supportIntent.putExtra(Intent.EXTRA_TEXT, message)
+            startActivity(supportIntent)
+        }
+        supportButton.setOnClickListener(supportButtonClickListener)
+
+        val agreementButton = findViewById<ImageButton>(R.id.agreement_button)
+        val agreementButtonClickListener = View.OnClickListener {
+            val web = "https://yandex.ru/legal/practicum_offer/"
+            val agreementIntent=Intent((Intent.ACTION_VIEW), Uri.parse(web))
+            startActivity(agreementIntent)
+        }
+        agreementButton.setOnClickListener(agreementButtonClickListener)
     }
 }
