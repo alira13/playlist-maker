9f7217b404d1c82b6057ec7344a70dbefa249c89
Alira13 <k.bezglasnaya@glorax.com>
2023-11-15 18:12:59 +0300

    Play-pause functions were added

--- app/src/main/java/com/example/playlistmaker/PlayerActivity.kt
+++ app/src/main/java/com/example/playlistmaker/PlayerActivity.kt
@@ -1,5 +1,6 @@
 package com.example.playlistmaker
 
+import android.media.MediaPlayer
 import android.os.Build.VERSION.SDK_INT
 import android.os.Bundle
 import android.widget.ImageButton
@@ -13,6 +14,18 @@ import java.text.SimpleDateFormat
 import java.util.Locale
 
 class PlayerActivity : AppCompatActivity() {
+
+    private val mediaPlayer = MediaPlayer()
+    private var playerState = STATE_DEFAULT
+
+    private lateinit var playControlButton: ImageButton
+
+    companion object {
+        private const val STATE_DEFAULT = 0
+        private const val STATE_PREPARED = 1
+        private const val STATE_PLAYING = 2
+        private const val STATE_PAUSED = 3
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_player)
@@ -36,6 +49,7 @@ class PlayerActivity : AppCompatActivity() {
         val releaseDate: TextView = findViewById(R.id.track_year_value)
         val primaryGenreName: TextView = findViewById(R.id.track_style_value)
         val country: TextView = findViewById(R.id.track_country_value)
+        playControlButton = findViewById(R.id.play_control_button)
 
         playerTrackName.text = track.trackName
         playerArtistName.text = track.artistName
@@ -57,5 +71,55 @@ class PlayerActivity : AppCompatActivity() {
             .placeholder(R.drawable.placeholder)
             .transform(RoundedCorners(trackImage.resources.getDimensionPixelSize(R.dimen.player_track_image_corner_radius)))
             .into(trackImage)
+
+        preparePlayer(track)
+        playControlButton.setOnClickListener { playerButtonControl() }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        pausePlayer()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mediaPlayer.release()
+    }
+
+    private fun playerButtonControl() {
+        when (playerState) {
+            STATE_PLAYING -> {
+                pausePlayer()
+            }
+
+            STATE_PAUSED, STATE_PREPARED -> {
+                startPlayer()
+            }
+        }
+    }
+
+    private fun preparePlayer(track: Track) {
+        mediaPlayer.setDataSource(track.previewUrl)
+        mediaPlayer.prepareAsync()
+        mediaPlayer.setOnPreparedListener {
+            playControlButton.isEnabled = true
+            playerState = STATE_PREPARED
+        }
+        mediaPlayer.setOnCompletionListener {
+            playControlButton.setImageResource(R.drawable.play_button);
+            playerState = STATE_PREPARED
+        }
+    }
+
+    private fun startPlayer() {
+        mediaPlayer.start()
+        playerState = STATE_PLAYING
+        playControlButton.setImageResource(R.drawable.pause_button);
+    }
+
+    private fun pausePlayer() {
+        mediaPlayer.pause()
+        playerState = STATE_PAUSED
+        playControlButton.setImageResource(R.drawable.play_button);
     }
 }
--- app/src/main/java/com/example/playlistmaker/Track.kt
+++ app/src/main/java/com/example/playlistmaker/Track.kt
@@ -13,7 +13,8 @@ data class Track(
     @SerializedName("collectionName") val collectionName: String,
     @SerializedName("releaseDate") val releaseDate: String,
     @SerializedName("primaryGenreName") val primaryGenreName: String,
-    @SerializedName("country") val country: String
+    @SerializedName("country") val country: String,
+    @SerializedName("previewUrl") val previewUrl : String
 ):Parcelable
 
 
