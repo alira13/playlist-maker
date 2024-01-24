4516b1904f062ea89f9823db368880ae6f954655
Alira13 <bezglasnaya@gmail.com>
2024-01-24 17:04:27 +0300

    Add PlayerListener to trackPlayer

--- app/src/main/java/com/example/playlistmaker/domain/player/TrackPlayer.kt
+++ app/src/main/java/com/example/playlistmaker/domain/player/TrackPlayer.kt
@@ -1,13 +1,10 @@
 package com.example.playlistmaker.domain.player
 
-import android.media.MediaPlayer
-
 interface TrackPlayer {
+    fun setListener(listener:PlayerListener)
     fun prepare(trackSource: String)
     fun play()
     fun pause()
     fun getCurrentTime(): String
     fun quit()
-    fun setOnCompletionListener(listener: MediaPlayer.OnCompletionListener)
-    fun setOnPreparedListener(listener: MediaPlayer.OnPreparedListener)
 }
