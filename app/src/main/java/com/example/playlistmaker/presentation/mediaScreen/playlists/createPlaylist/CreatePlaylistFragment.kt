9741788e6bc7ffddad8aa3a622594c641b2efda6
Alira13 <bezglasnaya@gmail.com>
2024-05-21 16:38:27 +0300

    Add logic for back btn in CreatePlaylist

--- app/src/main/java/com/example/playlistmaker/presentation/mediaScreen/playlists/createPlaylist/CreatePlaylistFragment.kt
+++ app/src/main/java/com/example/playlistmaker/presentation/mediaScreen/playlists/createPlaylist/CreatePlaylistFragment.kt
@@ -1,5 +1,6 @@
 package com.example.playlistmaker.presentation.mediaScreen.playlists.createPlaylist
 
+import android.net.Uri
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
@@ -7,10 +8,16 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.example.playlistmaker.R
 import com.example.playlistmaker.databinding.FragmentCreatePlaylistBinding
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 
 class CreatePlaylistFragment : Fragment() {
     var binding: FragmentCreatePlaylistBinding? = null
+
+    private var dialog: MaterialAlertDialogBuilder? = null
+    private var albumImageUri: Uri? = null
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -25,6 +32,28 @@ class CreatePlaylistFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        dialog = MaterialAlertDialogBuilder(requireContext()).apply {
+            setTitle(getString(R.string.complete_playlist_creation))
+            setMessage(getString(R.string.unsaved_data_losing))
+            setPositiveButton(getString(R.string.complete)) { _, _ ->
+                findNavController().navigateUp()
+            }
+            setNegativeButton(getString(R.string.cancel)) { _, _ ->
+            }
+        }
+
+        binding?.backButton?.setOnClickListener {
+            if (albumImageUri != null ||
+                !binding?.playlistNameTiEd?.text.isNullOrEmpty() ||
+                !binding?.descriptionTiEt?.text.isNullOrEmpty()
+            ) {
+                dialog?.show()
+            } else {
+                findNavController().navigateUp()
+            }
+        }
+
         addPlaylistNameTextWatcher()
     }
 
