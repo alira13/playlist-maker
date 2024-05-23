9f5f4cea2dd82a03882bf60f55c643e5b5d0b894
Alira13 <k.bezglasnaya@glorax.com>
2024-05-23 11:17:08 +0300

    Refactor NewPlaylistFragment

--- app/src/main/java/com/example/playlistmaker/presentation/newPlaylistScreen/NewPlaylistFragment.kt
+++ app/src/main/java/com/example/playlistmaker/presentation/newPlaylistScreen/NewPlaylistFragment.kt
@@ -12,6 +12,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.PickVisualMediaRequest
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
@@ -26,28 +27,50 @@ import java.io.FileOutputStream
 import java.util.Calendar
 
 class NewPlaylistFragment : Fragment() {
+    private val viewModel by viewModel<NewPlaylistViewModel>()
     var binding: FragmentNewPlaylistBinding? = null
 
-    private val viewModel by viewModel<NewPlaylistViewModel>()
+    private var onCloseDialog: MaterialAlertDialogBuilder? = null
+    var pickMedia: ActivityResultLauncher<PickVisualMediaRequest>? = null
 
-    private var dialog: MaterialAlertDialogBuilder? = null
     private var playlistImageUri: Uri? = null
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         binding = FragmentNewPlaylistBinding.inflate(inflater, container, false)
-
         return binding!!.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        dialog = MaterialAlertDialogBuilder(requireContext()).apply {
+        createOnCloseDialog()
+        createPickMedia()
+        addTextWatcher()
+
+        binding?.backButton?.setOnClickListener {
+            onBackClick()
+        }
+
+        binding?.playerTrackImage?.setOnClickListener {
+            onImageClick()
+        }
+
+        binding?.createButton?.setOnClickListener {
+            onCreateClick()
+        }
+    }
+
+    private fun createPickMedia(){
+        pickMedia =
+            registerForActivityResult(ActivityResultContracts.PickVisualMedia()) { uri ->
+                if (uri != null) {
+                    binding?.playerTrackImage?.setImageURI(uri)
+                    playlistImageUri = uri
+                }
+            }
+    }
+    private fun createOnCloseDialog(){
+        onCloseDialog = MaterialAlertDialogBuilder(requireContext()).apply {
             setTitle(getString(R.string.complete_playlist_creation))
             setMessage(getString(R.string.unsaved_data_losing))
             setPositiveButton(getString(R.string.complete)) { _, _ ->
@@ -56,55 +79,54 @@ class NewPlaylistFragment : Fragment() {
             setNegativeButton(getString(R.string.cancel)) { _, _ ->
             }
         }
-
-        binding?.backButton?.setOnClickListener {
-            if (playlistImageUri != null ||
-                !binding?.playlistNameTiEd?.text.isNullOrEmpty() ||
-                !binding?.descriptionTiEt?.text.isNullOrEmpty()
-            ) {
-                dialog?.show()
-            } else {
-                findNavController().navigateUp()
-            }
-        }
-
-        addPlaylistNameTextWatcher()
-
-        val pickMedia =
-            registerForActivityResult(ActivityResultContracts.PickVisualMedia()) { uri ->
-                if (uri != null) {
-                    binding?.playerTrackImage?.setImageURI(uri)
-                    playlistImageUri = uri
-                }
+    }
+    private fun addTextWatcher() {
+        binding?.playlistNameTiEd?.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
             }
 
-        binding?.playerTrackImage?.setOnClickListener {
-            pickMedia.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly))
-        }
-
-        binding?.createButton?.setOnClickListener {
-            val playlistName = binding?.playlistNameTiEd?.text.toString()
-            val playlistDescription = binding?.descriptionTiEt?.text.toString()
-
-            var playlistImageName: String? = null
-            if (playlistImageUri != null) {
-                playlistImageName = playlistName + Calendar.getInstance().time.toString() + ".jpg"
-                savePlaylistImage(playlistImageName)
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                binding?.createButton?.isEnabled = !s.isNullOrBlank()
             }
 
-            val playlist = NewPlaylist(playlistName, playlistDescription, playlistImageName)
-            viewModel.createPlaylist(playlist)
-
-            val toast = Toast(context)
-            toast.setGravity(Gravity.BOTTOM, 0, 0)
-            toast.duration = Toast.LENGTH_LONG
-            toast.setText("Плейлист $playlistName создан")
-            toast.show()
-
+            override fun afterTextChanged(s: Editable?) {
+            }
+        })
+    }
+    private fun onBackClick(){
+        if (playlistImageUri != null ||
+            !binding?.playlistNameTiEd?.text.isNullOrEmpty() ||
+            !binding?.descriptionTiEt?.text.isNullOrEmpty()
+        ) {
+            onCloseDialog?.show()
+        } else {
             findNavController().navigateUp()
         }
     }
+    private fun onImageClick(){
+        pickMedia?.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly))
+    }
+    private fun onCreateClick(){
+        val playlistName = binding?.playlistNameTiEd?.text.toString()
+        val playlistDescription = binding?.descriptionTiEt?.text.toString()
 
+        var playlistImageName: String? = null
+        if (playlistImageUri != null) {
+            playlistImageName = playlistName + Calendar.getInstance().time.toString() + ".jpg"
+            savePlaylistImage(playlistImageName)
+        }
+
+        val playlist = NewPlaylist(playlistName, playlistDescription, playlistImageName)
+        viewModel.createPlaylist(playlist)
+
+        val toast = Toast(context)
+        toast.setGravity(Gravity.BOTTOM, 0, 0)
+        toast.duration = Toast.LENGTH_LONG
+        toast.setText("Плейлист $playlistName создан")
+        toast.show()
+
+        findNavController().navigateUp()
+    }
     private fun savePlaylistImage(playlistImageName: String) {
         val filePath = File(
             requireActivity().getExternalFilesDir(Environment.DIRECTORY_PICTURES),
@@ -125,28 +147,12 @@ class NewPlaylistFragment : Fragment() {
         inputStream!!.close()
         outputStream.close()
     }
-
-    companion object {
-        private const val DIRECTORY = "playlists_images"
-        private const val QUALITY = 100
-    }
-
-    protected fun addPlaylistNameTextWatcher() {
-        binding?.playlistNameTiEd?.addTextChangedListener(object : TextWatcher {
-            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
-            }
-
-            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                binding?.createButton?.isEnabled = !s.isNullOrBlank()
-            }
-
-            override fun afterTextChanged(s: Editable?) {
-            }
-        })
-    }
-
     override fun onDestroyView() {
         super.onDestroyView()
         binding = null
+    }
+    companion object {
+        private const val DIRECTORY = "playlists_images"
+        private const val QUALITY = 100
     }
 }
